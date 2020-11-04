5d1af7675684c62719b310f52b21a7da8f3f7c4a
Frshr Technologies <64722744+WinMyRFP@users.noreply.github.com>
2020-11-04 14:41:49 +0530

    updates to heroku deployment

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(_dirname + '/dist/intershala-project'));
+app.use(express.static(__dirname + '/dist/intershala-project'));
 
 app.get('/*', (req,res) => {
-  res.sendFile(path.join(_dirname + '/dist/intershala-project/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/intershala-project/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
